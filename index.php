af5548a79c51788cd5b5ba7f72f2c83b9aed7474
Krzychurz <bartekrz66@gmail.com>
2022-10-06 15:09:04 +0200

    pierwsze wejscie

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Quiz</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+            $db = new mysqli("localhost","root","","quiz");
+            $page = 1;
+            $limit = 10;
+            $offset = ($page * $limit) - $limit;
+            $q = 'SELECT a.id,a.content,a.is_right,q.content FROM `answers` AS a JOIN questions as q on a.questions_id = q.id;';
+        ?>
+
+        <header>
+            <h3>Witaj w quizie</h3>
+        </header>
+    </body>
+</html>
