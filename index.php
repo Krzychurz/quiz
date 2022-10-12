96b027caa6178dc380ed56a00e4163cc3cbb7f8a
Krzychurz <bartekrz66@gmail.com>
2022-10-12 11:19:22 +0200

    jakotako działający kod

--- index.php
+++ index.php
@@ -1,31 +1,44 @@
-<html>
+<!DOCTYPE html>
+<html lang="pl">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
         <title>Quiz</title>
-        <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <?php
-            $db = new mysqli("localhost","root","","quiz");
-            $page = 1;
-            $limit = 10;
-            $offset = ($page * $limit) - $limit;
-            $q = 'SELECT a.id,a.content,a.is_right,q.content FROM `answers` AS a JOIN questions as q on a.questions_id = q.id;';
-        ?>
+            $con = new mysqli("localhost","root","","quiz");
+            $sql = "SELECT a.content AS answer, a.is_right, q.content AS question FROM answers AS a JOIN questions AS q ON a.questions_id = q.id";
 
-        <header>
-            <h3>Witaj w quizie</h3>
-        </header>
-        <section>
-            <?php
-                if($results = $db->query($q))
-                    while($row = $results->fetch_assoc())
-                    {
-                        echo"
-                            
-                        ";
-                    }
-            ?>
-        </section>
+            $res = $con->query($sql);
+            $row = $res->fetch_all(MYSQLI_ASSOC);
+
+            $true_i = NULL;
+
+                echo $row[$id]["question"]."<br>
+                <form action = 'index.php' method = 'GET'>  
+                ";
+                for ($i=0;$i<count($row);$i++)
+                {
+                    if($row[$i]['is_right'])
+                        $true_i = $i;
+                    echo"
+                        <input type='radio' name='radio_box' value='".($i+1)."'>
+                        ".$row[$i]['answer']."<br> ";
+                }
+                echo"
+                <input type='submit' value='Sprawdź'>
+                </form>";
+                
+                if(isset($_GET['radio_box']))
+                {
+                    if($_GET['radio_box'] == $row[$true_i]['is_right'])
+                        echo "Poprawna odpowiedź!<br>";
+                    else
+                        echo "Błędna odpowiedź, prawidłową odpowiedzią jest: ".$row[$true_i]['answer']."<br>";
+                }
+                else
+                    echo "Wybierz którąś z odpowiedzi!<br>";
+
+        ?>
     </body>
 </html>
