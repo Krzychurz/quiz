1341626991b072f60c5a869f191e46c31050b6fc
Krzychurz <bartekrz66@gmail.com>
2022-10-19 12:08:01 +0200

    szybka przesyłka

--- index.php
+++ index.php
@@ -7,43 +7,49 @@
     <body>
         <?php
             $con = new mysqli("localhost","root","","quiz");
-            $sql = "SELECT a.id AS aid, a.content AS answer, a.is_right, a.questions_id AS ident ,q.content AS question FROM answers AS a JOIN questions AS q ON a.questions_id = q.id";
-            #allah
+            $sql = "SELECT a.id AS a_id, a.content AS answer, a.is_right, a.questions_id AS ident ,q.content AS question FROM answers AS a JOIN questions AS q ON a.questions_id = q.id";
             $res = $con->query($sql);
             $row = $res->fetch_all(MYSQLI_ASSOC);
 
-            $punkty = $i = $j 0;
+            $punkty = $i = $j = 0;
+
             $id = 1;
             $right = [];
-            echo $row[$i]["question"]."<br>
+            echo $row[$id]["question"]."<br>
                 <form action = 'index.php' method = 'POST'>  
             ";
             while($id == $row[$i]['ident'])
             {
                 if($row[$i]['is_right'])
-                    $right[] = $row[$i]['aid'];
-                echo
-                "
-                    <input type='checkbox' name='check_box' value='$i'>
-                ".$row[$i]['answer']."<br> ";
+                {
+                    $right[] = $i;
+                    $j++;
+                }
+                echo "<input type='checkbox' name='$i' value='1'>".$row[$i]['answer']."<br>";
                 $i++;
             }
             echo
             "
                 <input type='submit' value='Sprawdź'>
             </form>";
-
-            if(isset($_POST['check_box']))
-                while($id == $row[$j]['ident'])
+            $k = 0;
+            while($id == $row[$k]['ident'] && $k<$j)
+            {
+                if(isset($_POST[$right[$k]]))
                 {
-                    if($_POST['check_box'] == $row[$right[$j]]['is_right'])
-                        $punkty++;
-                    $j++;
+                    for ($i=0;$i<$j;$i++)
+                    {
+                        if($row[$i]['is_right'] == $_POST[$i])
+                        {
+                            $punkty++;
+                        }
+                    }
                 }
+                $k++;
+            }
 
-            echo $punkty;
-            
-
+            echo "<br>Ilość punktów: $punkty";
+            $con->close();
         ?>
     </body>
 </html>
