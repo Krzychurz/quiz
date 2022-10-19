ad8f2c2dbf682e3d83b0534735d37b1bd54f5586
Krzychurz <bartekrz66@gmail.com>
2022-10-19 09:17:37 +0200

    przesłanie przed końcem lekcji

--- index.php
+++ index.php
@@ -7,44 +7,39 @@
     <body>
         <?php
             $con = new mysqli("localhost","root","","quiz");
-            $sql = "SELECT a.content AS answer, a.is_right, a.questions_id AS ident ,q.content AS question FROM answers AS a JOIN questions AS q ON a.questions_id = q.id";
+            $sql = "SELECT a.id AS aid, a.content AS answer, a.is_right, a.questions_id AS ident ,q.content AS question FROM answers AS a JOIN questions AS q ON a.questions_id = q.id";
 
             $res = $con->query($sql);
             $row = $res->fetch_all(MYSQLI_ASSOC);
 
+            $punkty = 0;
             $id = 1;
+            $i = 0;
+            $right = [];
+            echo $row[$i]["question"]."<br>
+                <form action = 'index.php' method = 'POST'>  
+            ";
+            while($id == $row[$i]['ident'])
             {
-                $true_i = 0;
-                echo $row[$id]["question"]."<br>
-                <form action = 'index.php' method = 'GET'>  
-                ";
-                while($id = $row[$i]['ident'])                //for ($i=0;$i<count($row[]);$i++)
-                {
-                    if($row[$i]['is_right'])
-                        $true_i = $i;
-                    echo"
-                        <input type='radio' name='radio_box' value='".($i+1)."'>
-                        ".$row[$i]['answer']."<br> ";
-                    $i++;
-                }
-                echo"
-                <input type='submit' name='submit' value='Sprawdź'>
-                </form>";
-                echo "WARTOSC SUBMIT: ";
-                echo $_GET['submit'];
-
-                if(isset($_GET['radio_box']))
-                {
-                    if($_GET['radio_box'] == $row[$true_i]['is_right'])
-                        echo "Poprawna odpowiedź!<br>";
-                    else
-                        echo "Błędna odpowiedź, prawidłową odpowiedzią jest: ".$row[$true_i]['answer']."<br>";
-                }
-                else
-                    {
-                        echo "Wybierz którąś odpowiedź!<br>";
-                    }
+                if($row[$i]['is_right'])
+                    $right[] = $row[$i]['aid'];
+                echo
+                "
+                    <input type='checkbox' name='check_box' value='$i'>
+                ".$row[$i]['answer']."<br> ";
+                $i++;
             }
+            echo
+            "
+                <input type='submit' value='Sprawdź'>
+            </form>";
+
+            if(isset($_POST['check_box']))
+                if($_POST['check_box'] == $row[$right]['is_right'])
+                    $punkty++;
+
+            echo $punkty;
+            
 
         ?>
     </body>
--- quiz.sql
+++ quiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 06 Paź 2022, 15:17
+-- Czas generowania: 19 Paź 2022, 09:16
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.1.6
 
@@ -39,9 +39,40 @@ CREATE TABLE `answers` (
 --
 
 INSERT INTO `answers` (`id`, `content`, `is_right`, `questions_id`) VALUES
-(1, 'tak', 1, 1),
-(2, 'nie', 0, 1),
-(3, 'prawie', 0, 1);
+(1, 'jeden', 1, 1),
+(2, 'dwa', 0, 1),
+(3, 'trzy', 0, 1),
+(7, '4', 1, 2),
+(8, '6', 0, 2),
+(9, '7', 0, 2),
+(10, '24 godziny', 1, 3),
+(11, '12 godzin', 0, 3),
+(12, 'pół dnia', 0, 3),
+(13, '7 dni', 1, 4),
+(14, '6 dni', 0, 4),
+(15, '5 dni', 0, 4),
+(16, '1/60', 1, 5),
+(17, '2', 0, 5),
+(18, '30', 0, 5),
+(19, 'W południe', 1, 6),
+(20, 'Po południu', 1, 6),
+(21, 'Wieczorem', 1, 6),
+(22, 'Rano', 0, 6),
+(23, 'Nie wychodzić ze szkoły', 0, 6),
+(24, 'Więcej niż jeden', 1, 7),
+(25, 'Więcej niż dwa', 1, 7),
+(26, 'Więcej trzy', 1, 7),
+(27, 'Mniej niż jeden', 0, 7),
+(28, 'Więcej niż trzy', 0, 7),
+(29, 'Dodatnie', 1, 8),
+(30, 'Ujemne', 1, 8),
+(31, 'Zmiennoprzecinkowe', 0, 8),
+(32, 'Więcej niż jeden', 1, 9),
+(33, 'Mniej niż dwieście', 1, 9),
+(34, 'Sto i koniec', 0, 9),
+(35, 'Patronem', 1, 10),
+(36, 'Dezydery', 1, 10),
+(37, 'Sienkiewicz', 0, 10);
 
 -- --------------------------------------------------------
 
@@ -59,7 +90,16 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`id`, `content`) VALUES
-(1, 'Czy 10 jest większe od 5?');
+(1, 'Ile naturalnych satelitów ma Ziemia?'),
+(2, 'Ile to 2 + 2?'),
+(3, 'Jak długo trwa dzień?'),
+(4, 'Ile dni jest w tygodniu?'),
+(5, 'Ile godzin ma minuta?'),
+(6, 'Jak wcześnie powinno się wstawać do szkoły?'),
+(7, 'Ile klawiszy ma standardowa myszka?'),
+(8, 'Jakie wartości przechowuje zmienna typu int?'),
+(9, 'Ile klawiszy ma standardowa klawiatura?'),
+(10, 'Jak się nazywa patron szkoły?');
 
 -- --------------------------------------------------------
 
@@ -72,6 +112,69 @@ CREATE TABLE `students` (
   `name` varchar(45) DEFAULT NULL,
   `surname` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `surname`) VALUES
+(1, 'Jan', 'Kowalski');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `students_has_tests`
+--
+
+CREATE TABLE `students_has_tests` (
+  `id` int(11) NOT NULL,
+  `students_id` int(11) NOT NULL,
+  `tests_has_answers_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `students_has_tests`
+--
+
+INSERT INTO `students_has_tests` (`id`, `students_id`, `tests_has_answers_id`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tests`
+--
+
+CREATE TABLE `tests` (
+  `id` int(11) NOT NULL,
+  `date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `tests`
+--
+
+INSERT INTO `tests` (`id`, `date`) VALUES
+(1, '2018-03-21');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tests_has_answers`
+--
+
+CREATE TABLE `tests_has_answers` (
+  `id` int(11) NOT NULL,
+  `tests_id` int(11) NOT NULL,
+  `answers_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Zrzut danych tabeli `tests_has_answers`
+--
+
+INSERT INTO `tests_has_answers` (`id`, `tests_id`, `answers_id`) VALUES
+(1, 1, 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -97,6 +200,28 @@ ALTER TABLE `students`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `students_has_tests`
+--
+ALTER TABLE `students_has_tests`
+  ADD PRIMARY KEY (`id`,`students_id`,`tests_has_answers_id`),
+  ADD KEY `fk_students_has_tests_has_answers_students1` (`students_id`),
+  ADD KEY `fk_students_has_tests_has_answers_tests_has_answers1` (`tests_has_answers_id`);
+
+--
+-- Indeksy dla tabeli `tests`
+--
+ALTER TABLE `tests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `tests_has_answers`
+--
+ALTER TABLE `tests_has_answers`
+  ADD PRIMARY KEY (`id`,`tests_id`,`answers_id`),
+  ADD KEY `fk_tests_has_answers_tests1` (`tests_id`),
+  ADD KEY `fk_tests_has_answers_answers1` (`answers_id`);
+
+--
 -- AUTO_INCREMENT dla zrzuconych tabel
 --
 
@@ -104,19 +229,37 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT dla tabeli `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT dla tabeli `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT dla tabeli `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `students_has_tests`
+--
+ALTER TABLE `students_has_tests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `tests`
+--
+ALTER TABLE `tests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `tests_has_answers`
+--
+ALTER TABLE `tests_has_answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Ograniczenia dla zrzutów tabel
@@ -127,6 +270,20 @@ ALTER TABLE `students`
 --
 ALTER TABLE `answers`
   ADD CONSTRAINT `fk_answers_questions` FOREIGN KEY (`questions_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ograniczenia dla tabeli `students_has_tests`
+--
+ALTER TABLE `students_has_tests`
+  ADD CONSTRAINT `fk_students_has_tests_has_answers_students1` FOREIGN KEY (`students_id`) REFERENCES `students` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_students_has_tests_has_answers_tests_has_answers1` FOREIGN KEY (`tests_has_answers_id`) REFERENCES `tests_has_answers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ograniczenia dla tabeli `tests_has_answers`
+--
+ALTER TABLE `tests_has_answers`
+  ADD CONSTRAINT `fk_tests_has_answers_answers1` FOREIGN KEY (`answers_id`) REFERENCES `answers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_tests_has_answers_tests1` FOREIGN KEY (`tests_id`) REFERENCES `tests` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
