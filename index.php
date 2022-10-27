072406231a8c05af420ae3caa266973d5a3cf71b
Krzychurz <bartekrz66@gmail.com>
2022-10-27 12:20:10 +0200

    raczej gotowy quiz

--- index.php
+++ index.php
@@ -15,83 +15,105 @@
             $res2 = $con->query($sql2);
             $row2 = $res2->fetch_all(MYSQLI_ASSOC);
 
+            $pkt = 0;
+            $spr = [];
+            $akt;
             $id_pytania;
             $losowe;
 
-            if(isset($_POST['id_pytania']))
+            if(isset($_POST['pkt']))
+                $pkt+=$_POST['pkt'];
+
+            if(isset($_POST['koniec']))
             {
-                $id_pytania = $_POST['id_pytania'];
-                $id_pytania++;
+                echo
+                "
+                    Koniec quizu.
+                    Twoja ilość punktów: $pkt
+                ";
             }
             else
-                $id_pytania = 1;
-
-            if(isset($_POST['losowe']))
-                $losowe=unserialize($_POST['losowe']);
-            else
             {
-                $losowe=range(0,count($row2)-1);
-                shuffle($losowe);
+                if(isset($_POST['id_pytania']))
+                {
+                    $id_pytania = $_POST['id_pytania'];
+                    $id_pytania++;
+                }
+                else
+                    $id_pytania = 1;
+
+                if(isset($_POST['losowe']))
+                    $losowe=unserialize($_POST['losowe']);
+                else
+                {
+                    $losowe=range(0,count($row2)-1);
+                    shuffle($losowe);
+                }
+
+                $ilosc_odp = 0;
+                while($id_pytania == $row[$ilosc_odp]['q_id'])
+                {
+                    $ilosc_odp++;
+                }
+
+                if(isset($_POST['spr']))
+                    $spr=unserialize($_POST['spr']);
+
+                echo $row2[$losowe[$id_pytania-1]]["content"]."<br>
+                <form action = 'index.php' method = 'POST'>";
+
+                $j=0;
+                $dobrze = 0;
+                for($i=0;$i<mysqli_num_rows($res);$i++)
+                {
+                    if($row[$i]['q_id'] == $row2[$losowe[$id_pytania-1]]['id'])
+                    {
+                        if(!$row[$i]['is_right'])
+                            $akt[$j] = 0;
+                        else
+                            $akt[$j] = 1;
+
+                        echo "<input type='hidden' name='$j' value=0>";
+                        echo "<input type='checkbox' name='$j' value=1>";
+                        echo $row[$i]['answer']."<br>";
+                        $j++;
+                    }
+                }
+
+                if(isset($_POST['spr']))
+                {	
+                    $dobrze=1;
+                    for($i=0;$i<count($spr);$i++)
+                    {
+                        if($spr[$i] != $_POST[$i])
+                        {
+                            $dobrze = 0;
+                            break;
+                        }
+                    }
+                }	
+                
+                if($dobrze)
+                    $pkt++;
+
+                //echo "<br>Punkty: $pkt<br>";
+
+                if($id_pytania == mysqli_num_rows($res2))
+                {
+                    echo "<input type='hidden' name='koniec' value='1'>";
+                }
+
+                $akt=serialize($akt);
+                $losowe=serialize($losowe);
+                echo
+                "
+                <input type='hidden' name='pkt' value='$pkt'>
+                <input type='hidden' name='spr' value='$akt'>
+                <input type='hidden' name='losowe' value='$losowe'>
+                <input type='hidden' name='id_pytania' value='$id_pytania'>
+                <input type='submit' value='Sprawdź'>
+                </form>";
             }
-
-            $ilosc_odp = 0;
-            while($id_pytania == $row[$ilosc_odp]['q_id'])
-            {
-                $ilosc_odp++;
-            }
-            
-            echo $losowe[$id_pytania-1]."<br>";
-            print_r($losowe);
-
-
-            echo $row2[$losowe[$id_pytania-1]]["content"]."<br>
-            <form action = 'index.php' method = 'POST'>";
-            
-			echo $row2[$id_pytania]['id'];
-			
-			$spr;
-			$j=0;
-			$dobrze = 1;
-            for($i=0;$i<mysqli_num_rows($res);$i++)
-			{
-				if($row[$i]['q_id'] == $row2[$losowe[$id_pytania-1]])
-				{
-					if(!$row[$i]['is_right'])
-						$spr[$j] = 0;
-					else
-						$spr[$j] = 1;
-					
-					echo "<input type='hidden' name='$j' value=0>";
-					echo "<input type='checkbox' name='$j' value=1>";
-					echo $row[$i]['answer']."<br>";
-					$j++;
-				}
-			}
-			
-			//print_r ($spr);
-			echo"<br>".count($spr)."<br>";
-			print_r ($_POST);
-			
-			if(isset($_POST[0]))
-			{	
-				for($i=0;$i<count($spr);$i++)
-				{
-					if($spr[$i] != $_POST[$i])
-					{
-						$dobrze = 0;
-						break;
-					}
-				}
-			}	
-			echo "<br>ODPOWIEDŹ".$dobrze;
-			
-            $losowe=serialize($losowe);
-            echo
-            "
-            <input type='hidden' name='losowe' value='$losowe'>
-            <input type='hidden' name='id_pytania' value='$id_pytania'>
-            <input type='submit' value='Sprawdź'>
-            </form>";
 
             /*
             if(isset($_POST['petla']))
